ccb97b72525d00dcf0c35c7e58b31b252cec2f5b
muskan <muskantuteja714@gmail.com>
2025-08-27 22:54:19 +0530

    27/8/25

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
+// sum question slove .....
+
 // let a =12
 // let b =23
 // console.log(a+b)
 
-
-
 // question concation/string
-let a = 12
-let b =" 67"
-// console.log(typeof(a+b))
-console.log("sum of 10 and 20 " +a +b)
-let c = 12
-let e = 67
-console.log("sum of 10 and 20 " +(c +e))
+// let a = 12
+// let b =" 67"
+// // console.log(typeof(a+b))
+// console.log("sum of 10 and 20 " +a +b)
+// let c = 12
+// let e = 67
+// console.log("sum of 10 and 20 " +(c +e))
+
+// let a = 12;
+// let b = 34;
+// console.log(a + b + "is sum of 10 and 29");
+
+// string ko name me converting karne jo hm number ka use karte hai h 
+// let age = Number(prompt("enter your name"))
+// console.log( age)
+
+
+// swap
+// let a =20
+// let b =34
+// let c 
+
+// c= a
+// a=b
+// b = c
+
+// let a = 10
+// let b = 30
+//  a = a + b // a = 40
+//  b = a - b // b = 10
+//  a = a - b //a 50
+
+
+let a = 20
+let b = 34
+
+[a,b] = [b,a
+
+  
+]
