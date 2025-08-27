7752a0ac72f7f9d6b7b544e5d0a83ccb69d6e933
muskan <muskantuteja714@gmail.com>
2025-08-27 19:27:55 +0530

    27/8/25 partices.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// let a =12
+// let b =23
+// console.log(a+b)
+
+
+
+// question concation/string
+let a = 12
+let b =" 67"
+// console.log(typeof(a+b))
+console.log("sum of 10 and 20 " +a +b)
+let c = 12
+let e = 67
+console.log("sum of 10 and 20 " +(c +e))
