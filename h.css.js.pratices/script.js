969412c007e766bb138e20ec92ff9ba1e2cf383b
muskan <muskantuteja714@gmail.com>
2025-09-01 15:54:13 +0530

    1/9/25

---
+++ h.css.js.pratices/script.js
@@ -0,0 +1,19 @@
+// function sayHello(){
+//   alert("me to thik hu ")
+// }
+
+// function changetext(){
+//   let h1 = document.querySelector("#mytext")
+//   h1.innerText = "Text change is successful"
+//   document.body.style.backgroundColor = "lightblue"
+//   console.log("h1")
+// }
+
+// function changecolor(){
+//  let randomColor = "#" +Math.floor(Math.random()*16777215).toString(16)
+
+//  document.body.style.backgroundColor = randomColor
+
+//  let h1 = document.querySelector(".color")
+//  h1.innerText = "background color:" + randomColor 
+// }
